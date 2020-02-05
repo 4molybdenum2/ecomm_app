f339bc72c19ca5e04098d3a5c424be5562e788f4
4molybdenum2 <tathagatapaul7@gmail.com>
2020-02-05 20:04:11 +0530

    added changes

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
 
-class App_Bar extends StatelessWidget {
+class App_Bar extends StatelessWidget with PreferredSizeWidget{
   @override
   Widget build(BuildContext context) {
-    return PreferredSize(
-      preferredSize: Size.fromHeight(50),
-      child: AppBar(
+    return AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.yellow,
         leading: IconButton(
           onPressed: () {},
           icon: Icon(Icons.arrow_back),
+          color: Colors.black,
         ),
-      ),
+
+        title: Text(
+          'Ecomm',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold
+            )
+        ),
+        actions: <Widget>[
+          
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.shopping_cart),
+            color: Colors.black,
+            ),
+
+          IconButton(
+            onPressed: null,
+            icon:Icon(Icons.more_vert),
+            color: Colors.black,
+            ),
+          
+        ],
+
     );
   }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'homepage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
