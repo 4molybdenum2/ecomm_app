f06198fef67ea400b9e353c6cf0335296af51e11
4molybdenum2 <tathagatapaul7@gmail.com>
2020-02-05 19:47:51 +0530

    fixed appbar code

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-class  App_Bar extends StatelessWidget {
+class  App_Bar extends StatelessWidget with PreferredSizeWidget{
   @override
   Widget build(BuildContext context) {
-    return AppBar(
+    return new AppBar(
       leading: IconButton(
         onPressed: (){},
         icon: Icon(Icons.arrow_back),
         ),
     );
   }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
