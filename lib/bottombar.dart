b11de4806012d44ae9bd86cffb7db5f0b3fdce98
kc611 <bcs_2019029@iiitm.ac.in>
2020-02-05 22:35:21 +0530

    changes bottombar

--- lib/bottombar.dart
+++ lib/bottombar.dart
@@ -6,80 +6,142 @@ class Bottom_Bar extends StatefulWidget {
 }
 
 class _Bottom_BarState extends State<Bottom_Bar> {
-  int _currentIndex = 0;
-  final List<Widget> _children = [
-
-  ];
-  void onTabTapped(int index) {
-
-    setState(() {
-      if(index!=2) {
-        _currentIndex = index;
-      }
-    });
-  }
+  int currentTab = 0; // to keep track of active tab index
+  final List<Widget> screens = [
+//    Dashboard(),
+//    Chat(),
+//    Profile(),
+//    Settings(),
+  ]; // to store nested tabs
+  final PageStorageBucket bucket = PageStorageBucket();
+//  Widget currentScreen = Dashboard(); // Our first view in viewport
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      onTap: onTabTapped, // new
-      currentIndex: _currentIndex, // new
+    return BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      notchMargin: 5,
+      child: Container(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                MaterialButton(
+                  minWidth: 40,
+                  onPressed: () {
+                    setState(() {
+//                      currentScreen =
+//                          Dashboard(); // if user taps on this dashboard tab will be active
+                      currentTab = 0;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.dashboard,
+                        color: currentTab == 0 ? Colors.red[400] : Colors.grey,
+                      ),
+                      Text(
+                        'Text1',
+                        style: TextStyle(
+                          color: currentTab == 0 ? Colors.red[400] : Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                MaterialButton(
+                  minWidth: 40,
+                  onPressed: () {
+                    setState(() {
+//                      currentScreen =
+//                          Chat(); // if user taps on this dashboard tab will be active
+                      currentTab = 1;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.chat,
+                        color: currentTab == 1 ? Colors.red[400] : Colors.grey,
+                      ),
+                      Text(
+                        'Text2',
+                        style: TextStyle(
+                          color: currentTab == 1 ? Colors.red[400] : Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
 
-      items: [
-        new BottomNavigationBarItem(
-          icon: Icon(
-              Icons.home,
-            color: Colors.red[400],
-          ),
-          title: Text(
-              'Home',
-            style: TextStyle(
-              color: Colors.red[400],
-            ),
-          ),
-        ),
-        new BottomNavigationBarItem(
-          icon: Icon(
-              Icons.mail,
-            color: Colors.red[400],
-          ),
-          title: Text(
-              'Messages',
-            style: TextStyle(
-              color: Colors.red[400],
-            ),
-          ),
-        ),
-        new BottomNavigationBarItem(
-            icon: Icon(
-                Icons.minimize,
-            ),
-            title: Text("")
-        ),
-        new BottomNavigationBarItem(
-            icon: Icon(
-                Icons.person,
-              color: Colors.red[400],
-            ),
-            title: Text(
-                'Profile',
-              style: TextStyle(
-                color: Colors.red[400],
-              ),
+            // Right Tab bar icons
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                MaterialButton(
+                  minWidth: 40,
+                  onPressed: () {
+                    setState(() {
+//                      currentScreen =
+//                          Profile(); // if user taps on this dashboard tab will be active
+                      currentTab = 2;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.dashboard,
+                        color: currentTab == 2 ? Colors.red[400] : Colors.grey,
+                      ),
+                      Text(
+                        'Text3',
+                        style: TextStyle(
+                          color: currentTab == 2 ? Colors.red[400] : Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                MaterialButton(
+                  minWidth: 40,
+                  onPressed: () {
+                    setState(() {
+//                      currentScreen =
+//                          Settings(); // if user taps on this dashboard tab will be active
+                      currentTab = 3;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.chat,
+                        color: currentTab == 3 ? Colors.red[400] : Colors.grey,
+                      ),
+                      Text(
+                        'Text4',
+                        style: TextStyle(
+                          color: currentTab == 3 ? Colors.red[400] : Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             )
+
+          ],
         ),
-         new BottomNavigationBarItem(
-             icon: Icon(
-                 Icons.ac_unit,
-               color: Colors.red[400],
-             ),
-             title: Text(
-                 'Cold Text',
-               style: TextStyle(
-                 color: Colors.red[400],
-               ),
-             )
-         )
-      ],
+      ),
     );
   }
 }
