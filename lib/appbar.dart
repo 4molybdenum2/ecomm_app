680e599f56f90b88349990996ec5f122e54731d8
4molybdenum2 <tathagatapaul7@gmail.com>
2020-02-05 19:17:16 +0530

    Made project

---
+++ lib/appbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class  App_Bar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        onPressed: (){},
+        icon: Icon(Icons.arrow_back),
+        ),
+    );
+  }
+}
