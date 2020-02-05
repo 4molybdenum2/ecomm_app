03877b008123c6b99d12bd78b22772ef3bd9d6e6
pranshu.s18 <pranshutejas@gmail.com>
2020-02-06 01:08:29 +0530

    AppBar Issue

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-class  App_Bar extends StatelessWidget {
+class App_Bar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      leading: IconButton(
-        onPressed: (){},
-        icon: Icon(Icons.arrow_back),
+    return PreferredSize(
+      preferredSize: Size.fromHeight(50),
+      child: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back),
         ),
+      ),
     );
   }
 }
