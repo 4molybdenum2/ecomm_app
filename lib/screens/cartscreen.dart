7dba0d709c0a61914984a8590c211d13138b1c29
4molybdenum2 <tathagatapaul7@gmail.com>
2020-02-13 13:42:10 +0530

    changes

--- lib/screens/cartscreen.dart
+++ lib/screens/cartscreen.dart
@@ -1,8 +1,6 @@
 import 'package:ecomm_app/database_helper/cartdatabase.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 
 class CartScreen extends StatefulWidget {
   @override
@@ -11,27 +9,25 @@ class CartScreen extends StatefulWidget {
 
 class _CartScreenState extends State<CartScreen> {
   var cartData;
-  int itemcount=0;
-  int currcount=0;
-  int totalcost=0;
-  int currcost=0;
-  int subtot=0;
-  int surcharge=0;
-  int delcharge=0;
-  int curritemqty=0;
-  String store="";
+  int itemcount = 0;
+  int currcount = 0;
+  int totalcost = 0;
+  int currcost = 0;
+  int subtot = 0;
+  int surcharge = 0;
+  int delcharge = 0;
+  int curritemqty = 0;
+  String store = "";
   var data = cartDatabaseProvider.db.getAllItem();
 
   @override
   void initState() {
     super.initState();
-    setState(() {
-
-    });
+    setState(() {});
   }
 
-  Widget _buildcartData(
-      productName, productQuantity, productPrice, productType,productID,productmrp) {
+  Widget _buildcartData(productName, productQuantity, productPrice, productType,
+      productID, productmrp) {
     return Container(
       margin: EdgeInsets.only(bottom: 20.0),
       decoration: BoxDecoration(
@@ -42,7 +38,7 @@ class _CartScreenState extends State<CartScreen> {
           borderRadius: BorderRadius.all(Radius.circular(10.0))),
       child: Padding(
         padding:
-        EdgeInsets.only(left: 10.0, right: 10.0, top: 15.0, bottom: 15.0),
+            EdgeInsets.only(left: 10.0, right: 10.0, top: 15.0, bottom: 15.0),
         child: GestureDetector(
             onTap: null,
             child: Row(
@@ -90,58 +86,59 @@ class _CartScreenState extends State<CartScreen> {
                     children: <Widget>[
                       IconButton(
                         color: Colors.red,
-                        onPressed: () async{
-                       //
+                        onPressed: () async {
+                          //
 
-                          currcount=int.parse(productQuantity) - 1;
-                          if(currcount==0){
-                            await cartDatabaseProvider.db.deleteItemWithId(productID);
-                          }else{
+                          currcount = int.parse(productQuantity) - 1;
+                          if (currcount == 0) {
+                            await cartDatabaseProvider.db
+                                .deleteItemWithId(productID);
+                          } else {
                             await cartDatabaseProvider.db.updateitem(new Item(
                                 name: productName,
                                 type: productType,
                                 quantity: currcount,
                                 storeprice: productPrice,
                                 mrp: productmrp,
-                                id: productID
-                            ));
+                                id: productID));
                           }
 
                           setState(() {
                             data = cartDatabaseProvider.db.getAllItem();
                           });
                         },
-                        icon: Icon(Icons.remove,size: 20,),
+                        icon: Icon(
+                          Icons.remove,
+                          size: 20,
+                        ),
                       ),
                       Text(
                         productQuantity.toString(),
-                        style: TextStyle(
-                          fontSize: 20
-                        ),
+                        style: TextStyle(fontSize: 20),
                       ),
                       IconButton(
                         color: Colors.red,
                         onPressed: () async {
-                          currcount=int.parse(productQuantity)+1;
+                          currcount = int.parse(productQuantity) + 1;
                           await cartDatabaseProvider.db.updateitem(new Item(
                               name: productName,
                               type: productType,
                               quantity: currcount,
                               storeprice: productPrice,
                               mrp: productmrp,
-                              id: productID
-                          ));
+                              id: productID));
 
                           setState(() {
                             data = cartDatabaseProvider.db.getAllItem();
                           });
                         },
-
-                        icon: Icon(Icons.add,size: 20,),
+                        icon: Icon(
+                          Icons.add,
+                          size: 20,
+                        ),
                       )
                     ],
                   ),
-
                 ])
               ],
             )),
@@ -149,43 +146,46 @@ class _CartScreenState extends State<CartScreen> {
     );
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red[400],
-      body: SafeArea(
-        child: ListView(
+        backgroundColor: Colors.red[400],
+        body: SafeArea(
+            child: ListView(
           children: <Widget>[
-
             SizedBox(height: 15.0),
             Padding(
               padding: EdgeInsets.only(left: 0.0),
-              child: Column(
-                children: <Widget>[
+              child: Column(children: <Widget>[
                 Text('Your Cart',
                     style: TextStyle(
                       color: Colors.white,
                       fontFamily: 'QuickSand',
                       fontSize: 30.0,
                       fontWeight: FontWeight.bold,
-                    )
-                    ),
-                    SizedBox(height: 10.0),
-                      Text('Subtotal: ₹$subtot' , style: textStyle,),
-                      Text('Delivery Charges: ₹$delcharge',style: textStyle,),
-                      Text('Taxes: ₹$surcharge',style: textStyle,),
-                      Text('Total: ₹$totalcost',style: textStyle,)
-
-
-              ]
-              ),
+                    )),
+                SizedBox(height: 10.0),
+                Text(
+                  'Subtotal: ₹$subtot',
+                  style: textStyle,
+                ),
+                Text(
+                  'Delivery Charges: ₹$delcharge',
+                  style: textStyle,
+                ),
+                Text(
+                  'Taxes: ₹$surcharge',
+                  style: textStyle,
+                ),
+                Text(
+                  'Total: ₹$totalcost',
+                  style: textStyle,
+                )
+              ]),
             ),
             SizedBox(
               height: 40.0,
             ),
-
-
             Container(
               height: MediaQuery.of(context).size.height - 185.0,
               decoration: BoxDecoration(
@@ -203,61 +203,50 @@ class _CartScreenState extends State<CartScreen> {
                     Padding(
                       padding: EdgeInsets.only(top: 30.0),
                       child: Container(
-                          child: FutureBuilder<List<Item>>(
-                            future: data,
-                            builder: (BuildContext context, AsyncSnapshot<List<Item>> snapshot) {
-                              if (snapshot.hasData) {
-                                return buildListView(snapshot);
-                              } else {
-                                return Center(child: CircularProgressIndicator());
-                              }
-                            },
-                          ),
+                        child: FutureBuilder<List<Item>>(
+                          future: data,
+                          builder: (BuildContext context,
+                              AsyncSnapshot<List<Item>> snapshot) {
+                            if (snapshot.hasData) {
+                              return buildListView(snapshot);
+                            } else {
+                              return Center(child: CircularProgressIndicator());
+                            }
+                          },
+                        ),
                       ),
                     ),
-
-
                   ]),
             ),
           ],
-        )
-      ),
-      floatingActionButton: FloatingActionButton(
-              onPressed: null,
-              backgroundColor: Colors.red[400],
-              foregroundColor: Colors.white,
-              child: Icon(Icons.shopping_basket),
-              )
-    );
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          backgroundColor: Colors.red[400],
+          foregroundColor: Colors.white,
+          child: Icon(Icons.shopping_basket),
+        ));
   }
 
   ListView buildListView(AsyncSnapshot<List<Item>> snapshot) {
     return ListView.builder(
-                                shrinkWrap: true,
-                                physics: BouncingScrollPhysics(),
-                                itemCount: snapshot.data.length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  Item item = snapshot.data[index];
-                                  return new Column(
-                                      children: <Widget>[
-                                      _buildcartData(
-                                      item.name,
-                                      item.quantity,
-                                      item.storeprice,
-                                      item.type,
-                                      item.id,
-                                      item.mrp)
-
-                                    ]);
-                                  },
-                              );
+      shrinkWrap: true,
+      physics: BouncingScrollPhysics(),
+      itemCount: snapshot.data.length,
+      itemBuilder: (BuildContext context, int index) {
+        Item item = snapshot.data[index];
+        return new Column(children: <Widget>[
+          _buildcartData(item.name, item.quantity, item.storeprice, item.type,
+              item.id, item.mrp)
+        ]);
+      },
+    );
   }
 }
 
- const TextStyle textStyle= TextStyle (
-    color: Colors.white,
-    fontFamily: 'QuickSand',
-    fontSize: 12.0,
-    fontWeight: FontWeight.bold,
-    
-  );
+const TextStyle textStyle = TextStyle(
+  color: Colors.white,
+  fontFamily: 'QuickSand',
+  fontSize: 12.0,
+  fontWeight: FontWeight.bold,
+);
